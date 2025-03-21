b00010e4b5f8474c09943badd3ebd77839c528d1
mozzamaru <kanghyun486111@gmail.com>
2025-03-22 02:26:43 +0900

    :sparkles: 하이퍼 슬라임 추가!

--- src/main/java/io/github/seamo/hyperCore/HyperCore.java
+++ src/main/java/io/github/seamo/hyperCore/HyperCore.java
@@ -17,6 +17,7 @@ public class HyperCore extends JavaPlugin {
         Bukkit.getPluginManager().registerEvents(new HyperZombie(config), this);
         Bukkit.getPluginManager().registerEvents(new HyperWeather(this, config), this);
         Bukkit.getPluginManager().registerEvents(new HyperPlayerRespawn(this), this);
+        Bukkit.getPluginManager().registerEvents(new HyperSlime(this), this);
 
         HyperWorldBorder worldBorder = new HyperWorldBorder(this, config);
         worldBorder.startBorderMovement();
