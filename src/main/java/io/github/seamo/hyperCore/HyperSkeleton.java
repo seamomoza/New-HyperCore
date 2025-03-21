b412b5d86ebb37423ea190eaf4858ac19dbcece0
mozzamaru <kanghyun486111@gmail.com>
2025-03-22 01:12:21 +0900

    :sparkles: 스켈레톤,좀비 극심한 버프

--- src/main/java/io/github/seamo/hyperCore/HyperSkeleton.java
+++ src/main/java/io/github/seamo/hyperCore/HyperSkeleton.java
@@ -3,12 +3,15 @@ package io.github.seamo.hyperCore;
 import org.bukkit.Location;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.*;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Skeleton;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.event.entity.EntityTargetEvent;
-import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -16,10 +19,12 @@ import org.bukkit.scheduler.BukkitRunnable;
 public class HyperSkeleton implements Listener {
     private final double arrowTick;
     private final double health;
+    private final int arrowNum;
 
     public HyperSkeleton(FileConfiguration config) {
         this.arrowTick = config.getDouble("Skeleton.skeleton-arrow-tick");
         this.health = config.getDouble("Skeleton.attri-health");
+        this.arrowNum = config.getInt("Skeleton.skeleton-arrow-num");
     }
 
     @EventHandler
@@ -27,7 +32,6 @@ public class HyperSkeleton implements Listener {
         if (event.getEntityType() == EntityType.SKELETON && event.getTarget() instanceof LivingEntity) {
             Skeleton skeleton = (Skeleton) event.getEntity();
             LivingEntity target = (LivingEntity) event.getTarget();
-
             new BukkitRunnable() {
                 @Override
                 public void run() {
@@ -38,8 +42,10 @@ public class HyperSkeleton implements Listener {
 
                     Location eyeLocation = skeleton.getEyeLocation();
                     Location targetLocation = target.getLocation().add(0, target.getHeight() / 2, 0);
-                    Arrow arrow = skeleton.launchProjectile(Arrow.class);
-                    arrow.setVelocity(targetLocation.toVector().subtract(eyeLocation.toVector()).normalize().multiply(2));
+                    for (int i = 0; i < arrowNum; i++) {
+                        Arrow arrow = skeleton.launchProjectile(Arrow.class);
+                        arrow.setVelocity(targetLocation.toVector().subtract(eyeLocation.toVector()).normalize().multiply(2));
+                    }
                 }
             }.runTaskTimer((Plugin) JavaPlugin.getPlugin(HyperCore.class), 0L, (long) arrowTick); // 1 tick = 0.05 seconds, 20 ticks = 1 second
         }
@@ -51,12 +57,13 @@ public class HyperSkeleton implements Listener {
             event.setCancelled(true);
         }
     }
+
     @EventHandler
     public void onSkeletonSpawn(EntitySpawnEvent event) {
         if (event.getEntityType() == EntityType.SKELETON) {
             Skeleton skeleton = (Skeleton) event.getEntity();
             skeleton.getAttribute(Attribute.MAX_HEALTH).setBaseValue(health);
-            skeleton.heal(health);
+            skeleton.setHealth(health);
         }
     }
 }
--- src/main/java/io/github/seamo/hyperCore/HyperZombie.java
+++ src/main/java/io/github/seamo/hyperCore/HyperZombie.java
@@ -5,8 +5,9 @@ import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Endermite;
 import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Silverfish;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Zombie;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -27,32 +28,41 @@ public class HyperZombie implements Listener {
     public void onZombieHit(EntityDamageByEntityEvent event) {
         if (event.getEntityType() == EntityType.ZOMBIE || event.getDamager().getType() == EntityType.ZOMBIE) {
             double spawnChance = config.getDouble("Zombie.spawn-Chance");
-            int spawnCount = config.getInt("Zombie.spawn-Silverfish");
+            int spawnCount = config.getInt("Zombie.spawn-EnderMite");
             if (random.nextDouble() * 100 < spawnChance) {
                 Location location = event.getEntity().getLocation();
-                for (int i = 0; i < spawnCount; i++) {
-                    Silverfish silverfish = (Silverfish) location.getWorld().spawnEntity(location, EntityType.SILVERFISH);
+                for (int i = 0; i <= spawnCount; i++) {
+                    Endermite endermite = (Endermite) location.getWorld().spawnEntity(location, EntityType.ENDERMITE);
+                    endermite.setTarget((LivingEntity) event.getDamager());
                 }
                 location.getWorld().spawnParticle(Particle.TOTEM_OF_UNDYING, location, 100);
                 location.getWorld().playSound(location, Sound.ENTITY_FIREWORK_ROCKET_BLAST, 1.0F, 1.0F);
             }
         }
     }
+
     @EventHandler
     public void onZombieSpawn(EntitySpawnEvent event) {
         if (event.getEntityType() == EntityType.ZOMBIE) {
-            double Health = config.getDouble("Zombie.attri-health");
-            double Damage = config.getDouble("Zombie.attri-damage");
+            double health = config.getDouble("Zombie.attri-health");
+            double damage = config.getDouble("Zombie.attri-damage");
             Zombie zombie = (Zombie) event.getEntity();
-            zombie.getAttribute(Attribute.MAX_HEALTH).setBaseValue(Health);
-            zombie.heal(Health);
-            zombie.getAttribute(Attribute.ATTACK_DAMAGE).setBaseValue(Damage);
-            Silverfish silverfish = (Silverfish) event.getEntity();
-            zombie.getAttribute(Attribute.KNOCKBACK_RESISTANCE).setBaseValue(1000000);
-            double da = config.getDouble("Zombie.zom-da");
-            double he = config.getDouble("Zombie.zom-he");
-            silverfish.getAttribute(Attribute.ATTACK_DAMAGE).setBaseValue(da);
-            silverfish.getAttribute(Attribute.MAX_HEALTH).setBaseValue(he);
+            zombie.getAttribute(Attribute.MAX_HEALTH).setBaseValue(health);
+            zombie.setHealth(health);
+            zombie.setNoDamageTicks(0);
+            zombie.getAttribute(Attribute.ATTACK_DAMAGE).setBaseValue(damage);
+            zombie.getAttribute(Attribute.KNOCKBACK_RESISTANCE).setBaseValue(1.0); // Set knockback resistance to 1.0 (100%)
+        }
+    }
+
+    @EventHandler
+    public void onEndermiteSpawn(EntitySpawnEvent event) {
+        if (event.getEntityType() == EntityType.ENDERMITE) {
+            double health = config.getDouble("Zombie.zom-he");
+            double attack = config.getDouble("Zombie.zom-da");
+            Endermite endermite = (Endermite) event.getEntity();
+            endermite.getAttribute(Attribute.MAX_HEALTH).setBaseValue(health);
+            endermite.getAttribute(Attribute.ATTACK_DAMAGE).setBaseValue(attack);
         }
     }
 }
