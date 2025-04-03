90ceffa05d853eaff08f010f5852c812941eae23
mozzamaru <kanghyun486111@gmail.com>
2025-04-03 23:24:38 +0900

    :sparkles: 하이퍼 골램 버프!

--- src/main/java/io/github/seamo/hyperCore/HyperGolem.java
+++ src/main/java/io/github/seamo/hyperCore/HyperGolem.java
@@ -1,10 +1,14 @@
 package io.github.seamo.hyperCore;
 
+import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.IronGolem;
+import org.bukkit.entity.Zombie;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntitySpawnEvent;
 
 public class HyperGolem implements Listener {
     @EventHandler
@@ -12,6 +16,17 @@ public class HyperGolem implements Listener {
         if (event.getEntityType() == EntityType.IRON_GOLEM) {
             Entity golem = event.getEntity();
             golem.spawnAt(event.getDamager().getLocation());
+        }
+    }
+    @EventHandler
+    public void onZombieSpawn(EntitySpawnEvent event) {
+        if (event.getEntityType() == EntityType.IRON_GOLEM) {
+            IronGolem zombie = (IronGolem) event.getEntity();
+            zombie.getAttribute(Attribute.MAX_HEALTH).setBaseValue(200);
+            zombie.setHealth(200);
+            zombie.setNoDamageTicks(0);
+            zombie.getAttribute(Attribute.ATTACK_DAMAGE).setBaseValue(10);
+            zombie.getAttribute(Attribute.KNOCKBACK_RESISTANCE).setBaseValue(1.0); // Set knockback resistance to 1.0 (100%)
         }
     }
 }
